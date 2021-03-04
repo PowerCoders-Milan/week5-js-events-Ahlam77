a3bece37e23bd77e6a1312367b1f3e9493cd760a
ahlam alshaba <ama1977ln@outlook.com>
2021-03-04 13:07:55 +0100

    done

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,12 @@
-// define your functions here
-
-// call the functions and connect them to an event
+var a = document.querySelector('a')
+ function changeBoldColor() {
+  var str = document.querySelectorAll('strong');
+  for (var i= 0; i < str.length; i++) {
+    str[i].style.color = "blue";
+  }
+}
+a.addEventListener('mouseenter', changeBoldColor);
+a.removeEventListener ("mouseleave", color);
+for (var i= 0; i < str.length; i++) {
+    str[i].style.color = "white";
+  }
