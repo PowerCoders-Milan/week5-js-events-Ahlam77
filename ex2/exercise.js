2963a87f4108fb9fa20ea80095d9e736ff28f802
ahlam alshaba <ama1977ln@outlook.com>
2021-03-04 14:53:12 +0100

    done

---
+++ ex2/exercise.js
@@ -0,0 +1,12 @@
+
+var w = window.innerWidth
+ document.documentElement.clientWidth
+ document.body.clientWidth;
+
+var h = window.innerHeight
+ document.documentElement.clientHeight
+ document.body.clientHeight;
+
+var x = document.getElementById("mimo");
+x.innerHTML = "The window is : "+ " Width :" + w + " pixels " + ", height: " + h + "pixels";
+
