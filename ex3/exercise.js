88173817797073fd3cb385d1b090d81abacc58ab
ahlam alshaba <ama1977ln@outlook.com>
2021-03-04 17:47:34 +0100

    done

---
+++ ex3/exercise.js
@@ -0,0 +1,23 @@
+var p = document.querySelector('p');
+var but1 = document.getElementById('up');
+var but2 = document.getElementById('down');
+var image = document.createElement('img');
+document.body.appendChild(image);
+var counter = 0 ;
+but1.addEventListener('click', function count(){
+    counter ++;
+    p.innerHTML = counter ;
+    if(counter == +10){
+   
+    image.setAttribute("src","ex3/emoji1.jpg");
+    }
+
+})
+but2.addEventListener('click', function count(){
+    counter --;
+    p.innerHTML = counter ;
+    if(counter == -10){
+        
+    image.setAttribute("src","ex3/image/imoji2.jpg");
+    }
+})
